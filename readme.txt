faba845859dcdd80ba49acd5d75957350566337e
artlosk <artlosk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-27 16:13:37 +0000

    git-svn-id: https://plugins.svn.wordpress.org/share-buttons/trunk@444299 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,122 @@
+=== Social Share Buttons for WordPress ===
+Contributors: Loskutnikov Artem (artlosk)
+Donate link: http://sbuttons.ru/donate-ru/ and http://sbuttons.ru/donate-en/
+Tags: social, network, jquery, share buttons, social buttons, facebook, googlebuzz, livejournal, mailru, odnoklassniki, twitter, vkontakte, yandex,
+Requires at least: 2.8
+Tested up to: 3.2
+Stable tag: 2.6
+
+The Share buttons, it is plugin for social networks. The plugin supports 9 social networking.
+
+== Description ==
+
+The Share buttons, it is plugin for social networks. The plugin supports 9 social networking. 
+There are Vkontakte, Odnoklassniki.ru, Mail.ru, LiveJournal, Yandex russian social buttons and there are Facebook, Twitter, Google-Buzz, Google-Plus english social buttons. 
+Settings include show/hide buttons, sort buttons, position buttons and e.t.c.
+This plugin is written using AJAX and Jquery. The plugin supports 2 languages: English and Russian
+
+[FAQ]
+-RUS (http://sbuttons.ru/tutorial-ru/installation-ru/)
+-ENG (http://sbuttons.ru/tutorial-en/installation-en/) 
+
+To disable the main output social share buttons and use only shortcode '[share-buttons]' in Post/Page, open share-buttons.php and comment line 103
+'add_filter('the_content', array(&$this, 'place_button'));' <- this comment
+
+== Languages ==
+
+* English - default
+* Russina(ru_RU)
+
+== Installation ==
+
+1. Unzip the file
+2. Upload it to wp-content/plugins
+3. Activate it from the plugins section
+4. Go to the FTP "wp-content/plugins/share_buttons/upload/ and change chmod folder "/UPLOADS/". Your logo will store in this folder.
+NOTE:
+or upload via FTP manually
+
+== Screenshots ==
+
+1. Upload picture for site-logo
+2. Sort Buttons, Choose style, Enable/Disabled buttons
+3. Margins top and bottom
+4. Header text before social buttons
+5. Position Share Buttons
+6. Twitter via "@"
+7. Like button - Vkontakte
+8. Like button - Facebook
+9. Like button - Mailru
+== Changelog ==
+
+[2.6]
+
+- Add Yandex button
+- Add Google Plus button (test mode)
+- Optimize code and database (add options 'buttons_sort' and 'buttons_show' instead of twitter_show, facebook_show and e.t.c)
+- Delete Original and Original Count styles (I think they are not needed)
+- Add Mini style
+- Fix problem library mb_string
+- Fix buttons border
+- Change sort for social buttons
+- Fix display buttons in Page, Post, Home
+- Fix <nofollow> and <noindex>
+- Add Margins top and bottom for block social buttons
+- Add Shortcode in Post/Page '[share-button]' (but main block output enabled =( )
+
+[2.5]
+
+- Add Livejournal button
+- Add Google-Buzz button
+- Create Site for this plugin http://sbuttons.ru
+- Update russian language
+- Add text before social buttons
+- Change Sort
+and e.t.c (few changes)
+
+[2.2]
+
+Fixed output "Like button for Vkontakte" when displayed in loop of posts. 
+Now the button's container with a unique ID, for example &lt;div&gt; id='vk_like_$post->ID'>&lt;/div&gt;
+
+[2.1]
+
+Fixed URL for page
+
+[2.0]
+
+- Fixed upload file "logo.png"
+- Fixed output description and title
+- Fixed Facebook button with count
+- Optimized plugin
+- Change interface
+- Change structure folder, files and php code
+- Add fieldset "Sorting buttons"
+- Add 6 pack icons
+- Add Mail.ru button "Like"
+- Add Share Buttons for Frontend(Home page)
+
+[1.2.2]
+
+Fixed get url for twitter, mailru buttons.
+"$url = get_permalink($post->ID);"
+
+[1.2.1]
+Fixed output <meta>. Output without html.
+
+if upload logo is failed, please, change chmod folder "uploads" and chmod file "logo.png" to upload logo for stie
+or upload via FTP manually (this script while in the process)
+
+[1.2]
+ - Previously, the plugin inscribe title and description, which was introduced Platinum SEO Pack plugin and like it. Now the plugin takes from post and cuts 300 characters to description. <meta name="description" content="$description" />
+ - Optimized scripts social networking.
+
+[1.1]
+Fixed upload logo
+
+[1.0]
+Plugin release.
+
+== Feature ==
+- Add many share buttons
+- Create special site for this plugin
---
+++ share/share_customize_type.php
@@ -0,0 +1,239 @@
+<?php
+
+	global $post;
+	$locale = substr(get_locale(),0,2);
+	$title = esc_js($post->post_title);
+	$url = get_permalink($post->ID);
+	$via = get_option('twitter_via');
+//        $vk_text = __('Share',$this->plugin_domain);
+	$customize_type = get_option('opt_customize_type');
+	$facebook	="";
+	$twitter	="";
+	$mailru		="";
+	$odkl		="";
+	$vkontakte	="";
+	$vk_type	="";
+	$livejournal	="";
+	$googlebuzz	="";
+	$yandex		="";
+
+	if($via!='') { $via_new='&via='.$via; } else { $via_new = ''; }
+
+	
+	if($customize_type=='classic') {
+
+		/* Mail.ru */
+		$mailru .= "<div class='mailru'>";
+		$mailru .= "<noindex><a rel='nofollow' title='".__('Share to MyWorld', $this->plugin_domain)."' href=\"#mailru\" name=\"mailru\" onclick=\"new_window('http://connect.mail.ru/share?share_url=$url');\">";
+		$mailru .= "<img src='".$this->plugin_url."images/social/classic/mailru.png' alt='".__('Share to MyWorld', $this->plugin_domain)."'/></a></noindex></div>";
+
+		/* Odnoklassniki */
+                $odkl .= "<div class='odkl'>";
+		$odkl .= "<noindex><a rel='nofollow' title='".__('Share to Odnoklassniki', $this->plugin_domain)."' href=\"#odnoklassniki\" name=\"odnoklassniki\" onclick=\"new_window('http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1&st._surl=$url');\">";
+		$odkl .= "<img src='".$this->plugin_url."images/social/classic/odnoklassniki.png' alt='".__('Share to Odnoklassniki', $this->plugin_domain)."'/></a></noindex></div>";
+
+		/* Tweet */
+		$twitter .= "<div class='twitter'>";
+		$twitter .= "<noindex><a rel='nofollow' title='".__('Share to Twitter', $this->plugin_domain)."' href=\"#twitter\" name=\"twitter\" onclick=\"new_window('http://twitter.com/share?&text=$title%20-%20&url=$url$via_new');\">";
+		$twitter .= "<img src='".$this->plugin_url."images/social/classic/twitter.png' alt='".__('Share to Twitter', $this->plugin_domain)."'/></a></noindex></div>";
+
+		/* Facebook */
+		$facebook .= "<div class='fb'>";
+		$facebook .= "<noindex><a rel='nofollow' title='".__('Share to Facebook', $this->plugin_domain)."' href=\"#facebook\" name=\"facebook\" onclick=\"new_window('http://www.facebook.com/sharer.php?u=$url');\">";
+		$facebook .= "<img src='".$this->plugin_url."images/social/classic/facebook.png' alt='".__('Share to Facebook', $this->plugin_domain)."'/></a></noindex></div>";
+
+		$livejournal .= "<div class='livejournal'>";
+		$livejournal .= "<noindex><a rel='nofollow' title='".__('Share to LiveJournal', $this->plugin_domain)."' href=\"http://www.livejournal.com/update.bml?event=".$url."&subject=".$title."\" target=\"_blank\" name =\"livejournal\"/>";
+		$livejournal .= "<img src='".$this->plugin_url."images/social/classic/livejournal.png' alt='".__('Share to LiveJournal', $this->plugin_domain)."'/></a></noindex></div>";
+
+
+		$googlebuzz .= "<div class='googlebuzz'>";
+		$googlebuzz .= "<noindex><a rel='nofollow' title='".__('Share to Google Buzz', $this->plugin_domain)."' href=\"http://www.google.com/buzz/post?message=".$title."&url=".$url."\">";
+		$googlebuzz .= "<img src='".$this->plugin_url."images/social/classic/googlebuzz.png' alt='".__('Share to Google Buzz', $this->plugin_domain)."'/></a></noindex></div>";
+
+		$yandex .= "<div class='yandex'>";
+		$yandex .= "<noindex><a rel='nofollow' title='".__('Share to Yandex', $this->plugin_domain)."' href=\"http://share.yandex.ru/go.xml?service=yaru&url=".$url."&title=".$title."\">";
+		$yandex .= "<img src='".$this->plugin_url."images/social/classic/yandex.png' /></a></noindex></div>";
+
+		$googleplus .= "<div class='googleplus'>";
+		$googleplus .= "<noindex><a rel='nofollow' title='".__('Share to Google Plus', $this->plugin_domain)."' href=\"https://m.google.com/app/plus/x/?v=compose&content=".$title." - ".$url."\" onclick=\"window.open('https://m.google.com/app/plus/x/?v=compose&content=".$title." - ".$url."','gplusshare','width=450,height=300,left='+(screen.availWidth/2-225)+',top='+(screen.availHeight/2-150)+'');return false;\">";
+		$googleplus .= "<img src='".$this->plugin_url."images/social/classic/googleplus.png' alt='".__('Share to Google Plus', $this->plugin_domain)."'/></a></noindex></div>";
+
+		/* Vkontakte */
+		$vk_type .= "custom";
+		$vk_text = '<img src="'.$this->plugin_url.'images/social/classic/vkontakte.png" title="'.__("Share to Vkontakte", $this->plugin_domain).'" alt="'.__("Share to Vkontakte", $this->plugin_domain).'"/>';
+		$vkontakte .= "<div class='vk'>";
+
+	} else if($customize_type=='mini') {
+
+		/* Mail.ru */
+		$mailru .= "<div class='mailru-mini'>";
+		$mailru .= "<noindex><a rel='nofollow' title='".__('Share to MyWorld', $this->plugin_domain)."' href=\"#mailru\" name=\"mailru\" onclick=\"new_window('http://connect.mail.ru/share?share_url=$url');\">";
+		$mailru .= "<img src='".$this->plugin_url."images/social/mini/mailru.png' alt='".__('Share to MyWorld', $this->plugin_domain)."'/></a></noindex></div>";
+
+		/* Odnoklassniki */
+                $odkl .= "<div class='odkl-mini'>";
+		$odkl .= "<noindex><a rel='nofollow' title='".__('Share to Odnoklassniki', $this->plugin_domain)."' href=\"#odnoklassniki\" name=\"odnoklassniki\" onclick=\"new_window('http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1&st._surl=$url');\">";
+		$odkl .= "<img src='".$this->plugin_url."images/social/mini/odnoklassniki.png' alt='".__('Share to Odnoklassniki', $this->plugin_domain)."'/></a></noindex></div>";
+
+		/* Tweet */
+		$twitter .= "<div class='twitter-mini'>";
+		$twitter .= "<noindex><a rel='nofollow' title='".__('Share to Twitter', $this->plugin_domain)."' href=\"#twitter\" name=\"twitter\" onclick=\"new_window('http://twitter.com/share?&text=$title%20-%20&url=$url$via_new');\">";
+		$twitter .= "<img src='".$this->plugin_url."images/social/mini/twitter.png' alt='".__('Share to Twitter', $this->plugin_domain)."'/></a></noindex></div>";
+
+		/* Facebook */
+		$facebook .= "<div class='fb-mini'>";
+		$facebook .= "<noindex><a rel='nofollow' title='".__('Share to Facebook', $this->plugin_domain)."' href=\"#facebook\" name=\"facebook\" onclick=\"new_window('http://www.facebook.com/sharer.php?u=$url');\">";
+		$facebook .= "<img src='".$this->plugin_url."images/social/mini/facebook.png' alt='".__('Share to Facebook', $this->plugin_domain)."'/></a></noindex></div>";
+
+		$livejournal .= "<div class='livejournal-mini'>";
+		$livejournal .= "<noindex><a rel='nofollow' title='".__('Share to LiveJournal', $this->plugin_domain)."' href=\"http://www.livejournal.com/update.bml?event=".$url."&subject=".$title."\" target=\"_blank\" name =\"livejournal\"/>";
+		$livejournal .= "<img src='".$this->plugin_url."images/social/mini/livejournal.png' alt='".__('Share to LiveJournal', $this->plugin_domain)."'/></a></noindex></div>";
+
+
+		$googlebuzz .= "<div class='googlebuzz-mini'>";
+		$googlebuzz .= "<noindex><a rel='nofollow' title='".__('Share to Google Buzz', $this->plugin_domain)."' href=\"http://www.google.com/buzz/post?message=".$title."&url=".$url."\">";
+		$googlebuzz .= "<img src='".$this->plugin_url."images/social/mini/googlebuzz.png' alt='".__('Share to Google Buzz', $this->plugin_domain)."'/></a></noindex></div>";
+
+		$yandex .= "<div class='yandex-mini'>";
+		$yandex .= "<noindex><a rel='nofollow' title='".__('Share to Yandex', $this->plugin_domain)."' href=\"http://share.yandex.ru/go.xml?service=yaru&url=".$url."&title=".$title."\">";
+		$yandex .= "<img src='".$this->plugin_url."images/social/mini/yandex.png' alt='".__('Share to Yandex', $this->plugin_domain)."'/></a></noindex></div>";
+
+		$googleplus .= "<div class='googleplus-mini'>";
+		$googleplus .= "<noindex><a rel='nofollow' title='".__('Share to Google Plus', $this->plugin_domain)."' href=\"https://m.google.com/app/plus/x/?v=compose&content=".$title." - ".$url."\" onclick=\"window.open('https://m.google.com/app/plus/x/?v=compose&content=".$title." - ".$url."','gplusshare','width=450,height=300,left='+(screen.availWidth/2-225)+',top='+(screen.availHeight/2-150)+'');return false;\">";
+		$googleplus .= "<img src='".$this->plugin_url."images/social/mini/googleplus.png' alt='".__('Share to Google Plus', $this->plugin_domain)."'/></a></noindex></div>";
+
+		/* Vkontakte */
+		$vk_type .= "custom";
+		$vk_text = '<img src="'.$this->plugin_url.'images/social/mini/vkontakte.png" title="'.__("Share to Vkontakte", $this->plugin_domain).'" alt="'.__("Share to Vkontakte", $this->plugin_domain).'"/>';
+		$vkontakte .= "<div class='vk-mini'>";
+
+	} else if($customize_type=='soft_rect') {
+
+		/* Mail.ru */
+		$mailru .= "<div class='mailru'>";
+		$mailru .= "<noindex><a rel='nofollow' title='".__('Share to MyWorld', $this->plugin_domain)."' href=\"#mailru\" name=\"mailru\" onclick=\"new_window('http://connect.mail.ru/share?share_url=$url');\">";
+		$mailru .= "<img src='".$this->plugin_url."images/social/soft_rect/mailru.png' alt='".__('Share to MyWorld', $this->plugin_domain)."'/></a></noindex></div>";
+
+		/* Odnoklassniki */
+                $odkl .= "<div class='odkl'>";
+		$odkl .= "<noindex><a rel='nofollow' title='".__('Share to Odnoklassniki', $this->plugin_domain)."' href=\"#odnoklassniki\" name=\"odnoklassniki\" onclick=\"new_window('http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1&st._surl=$url');\">";
+		$odkl .= "<img src='".$this->plugin_url."images/social/soft_rect/odnoklassniki.png' alt='".__('Share to Odnoklassniki', $this->plugin_domain)."'/></a></noindex></div>";
+
+		/* Tweet */
+		$twitter .= "<div class='twitter'>";
+		$twitter .= "<noindex><a rel='nofollow' title='".__('Share to Twitter', $this->plugin_domain)."' href=\"#twitter\" name=\"twitter\" onclick=\"new_window('http://twitter.com/share?&text=$title%20-%20&url=$url$via_new');\">";
+		$twitter .= "<img src='".$this->plugin_url."images/social/soft_rect/twitter.png' alt='".__('Share to Twitter', $this->plugin_domain)."'/></a></noindex></div>";
+
+		/* Facebook */
+		$facebook .= "<div class='fb'>";
+		$facebook .= "<noindex><a rel='nofollow' title='".__('Share to Facebook', $this->plugin_domain)."' href=\"#facebook\" name=\"facebook\" onclick=\"new_window('http://www.facebook.com/sharer.php?u=$url');\">";
+		$facebook .= "<img src='".$this->plugin_url."images/social/soft_rect/facebook.png' alt='".__('Share to Facebook', $this->plugin_domain)."'/></a></noindex></div>";
+
+		$livejournal .= "<div class='livejournal'>";
+		$livejournal .= "<noindex><a rel='nofollow' title='".__('Share to LiveJournal', $this->plugin_domain)."' href=\"http://www.livejournal.com/update.bml?event=".$url."&subject=".$title."\" target=\"_blank\" name =\"livejournal\"/>";
+		$livejournal .= "<img src='".$this->plugin_url."images/social/soft_rect/livejournal.png' alt='".__('Share to LiveJournal', $this->plugin_domain)."'/></a></noindex></div>";
+
+
+		$googlebuzz .= "<div class='googlebuzz'>";
+		$googlebuzz .= "<noindex><a rel='nofollow' title='".__('Share to Google Buzz', $this->plugin_domain)."' href=\"http://www.google.com/buzz/post?message=".$title."&url=".$url."\">";
+		$googlebuzz .= "<img src='".$this->plugin_url."images/social/soft_rect/googlebuzz.png' alt='".__('Share to Google Buzz', $this->plugin_domain)."'/></a></noindex></div>";
+
+		$yandex .= "<div class='yandex'>";
+		$yandex .= "<noindex><a rel='nofollow' title='".__('Share to Yandex', $this->plugin_domain)."' href=\"http://share.yandex.ru/go.xml?service=yaru&url=".$url."&title=".$title."\">";
+		$yandex .= "<img src='".$this->plugin_url."images/social/soft_rect/yandex.png' alt='".__('Share to Yandex', $this->plugin_domain)."'/></a></noindex></div>";
+
+		$googleplus .= "<div class='googleplus'>";
+		$googleplus .= "<noindex><a rel='nofollow' title='".__('Share to Google Plus', $this->plugin_domain)."' href=\"https://m.google.com/app/plus/x/?v=compose&content=".$title." - ".$url."\" onclick=\"window.open('https://m.google.com/app/plus/x/?v=compose&content=".$title." - ".$url."','gplusshare','width=450,height=300,left='+(screen.availWidth/2-225)+',top='+(screen.availHeight/2-150)+'');return false;\">";
+		$googleplus .= "<img src='".$this->plugin_url."images/social/soft_rect/googleplus.png' alt='".__('Share to Google Plus', $this->plugin_domain)."'/></a></noindex></div>";
+
+		/* Vkontakte */
+		$vk_type .= "custom";
+		$vk_text = '<img src="'.$this->plugin_url.'images/social/soft_rect/vkontakte.png" title="'.__("Share to Vkontakte", $this->plugin_domain).'" alt="'.__("Share to Vkontakte", $this->plugin_domain).'"/>';
+		$vkontakte .= "<div class='vk'>";
+
+	} else if($customize_type=='soft_round') {
+
+		/* Mail.ru */
+		$mailru .= "<div class='mailru'>";
+		$mailru .= "<noindex><a rel='nofollow' title='".__('Share to MyWorld', $this->plugin_domain)."' href=\"#mailru\" name=\"mailru\" onclick=\"new_window('http://connect.mail.ru/share?share_url=$url');\">";
+		$mailru .= "<img src='".$this->plugin_url."images/social/soft_round/mailru.png' alt='".__('Share to MyWorld', $this->plugin_domain)."'/></a></noindex></div>";
+
+		/* Odnoklassniki */
+                $odkl .= "<div class='odkl'>";
+		$odkl .= "<noindex><a rel='nofollow' title='".__('Share to Odnoklassniki', $this->plugin_domain)."' href=\"#odnoklassniki\" name=\"odnoklassniki\" onclick=\"new_window('http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1&st._surl=$url');\">";
+		$odkl .= "<img src='".$this->plugin_url."images/social/soft_round/odnoklassniki.png' alt='".__('Share to Odnoklassniki', $this->plugin_domain)."'/></a></noindex></div>";
+
+		/* Tweet */
+		$twitter .= "<div class='twitter'>";
+		$twitter .= "<noindex><a rel='nofollow' title='".__('Share to Twitter', $this->plugin_domain)."' href=\"#twitter\" name=\"twitter\" onclick=\"new_window('http://twitter.com/share?&text=$title%20-%20&url=$url$via_new');\">";
+		$twitter .= "<img src='".$this->plugin_url."images/social/soft_round/twitter.png' alt='".__('Share to Twitter', $this->plugin_domain)."'/></a></noindex></div>";
+
+		/* Facebook */
+		$facebook .= "<div class='fb'>";
+		$facebook .= "<noindex><a rel='nofollow' title='".__('Share to Facebook', $this->plugin_domain)."' href=\"#facebook\" name=\"facebook\" onclick=\"new_window('http://www.facebook.com/sharer.php?u=$url');\">";
+		$facebook .= "<img src='".$this->plugin_url."images/social/soft_round/facebook.png' alt='".__('Share to Facebook', $this->plugin_domain)."'/></a></noindex></div>";
+
+		$livejournal .= "<div class='livejournal'>";
+		$livejournal .= "<noindex><a rel='nofollow' title='".__('Share to LiveJournal', $this->plugin_domain)."' href=\"http://www.livejournal.com/update.bml?event=".$url."&subject=".$title."\" target=\"_blank\" name =\"livejournal\"/>";
+		$livejournal .= "<img src='".$this->plugin_url."images/social/soft_round/livejournal.png' alt='".__('Share to LiveJournal', $this->plugin_domain)."'/></a></noindex></div>";
+
+
+		$googlebuzz .= "<div class='googlebuzz'>";
+		$googlebuzz .= "<noindex><a rel='nofollow' title='".__('Share to Google Buzz', $this->plugin_domain)."' href=\"http://www.google.com/buzz/post?message=".$title."&url=".$url."\">";
+		$googlebuzz .= "<img src='".$this->plugin_url."images/social/soft_round/googlebuzz.png' alt='".__('Share to Google Buzz', $this->plugin_domain)."'/></a></noindex></div>";
+
+		$yandex .= "<div class='yandex'>";
+		$yandex .= "<noindex><a rel='nofollow' title='".__('Share to Yandex', $this->plugin_domain)."' href=\"http://share.yandex.ru/go.xml?service=yaru&url=".$url."&title=".$title."\">";
+		$yandex .= "<img src='".$this->plugin_url."images/social/soft_round/yandex.png' alt='".__('Share to Yandex', $this->plugin_domain)."'/></a></noindex></div>";
+
+		$googleplus .= "<div class='googleplus'>";
+		$googleplus .= "<noindex><a rel='nofollow' title='".__('Share to Google Plus', $this->plugin_domain)."' href=\"https://m.google.com/app/plus/x/?v=compose&content=".$title." - ".$url."\" onclick=\"window.open('https://m.google.com/app/plus/x/?v=compose&content=".$title." - ".$url."','gplusshare','width=450,height=300,left='+(screen.availWidth/2-225)+',top='+(screen.availHeight/2-150)+'');return false;\">";
+		$googleplus .= "<img src='".$this->plugin_url."images/social/soft_round/googleplus.png' alt='".__('Share to Google Plus', $this->plugin_domain)."'/></a></noindex></div>";
+
+		/* Vkontakte */
+		$vk_type .= "custom";
+		$vk_text = '<img src="'.$this->plugin_url.'images/social/soft_round/vkontakte.png" title="'.__("Share to Vkontakte", $this->plugin_domain).'" alt="'.__("Share to Vkontakte", $this->plugin_domain).'"/>';
+		$vkontakte .= "<div class='vk'>";
+	}
+
+		$vkontakte .="<script type=\"text/javascript\">\r\n<!--\r\ndocument.write(VK.Share.button(\r\n{\r\n";
+		$vkontakte .= "  url: '$url',\r\n";
+		$vkontakte .= "  title: '$title',\r\n";
+		$vkontakte .= "  description: '$descr'";
+		$vkontakte .= $noparse == 'true' ? ",\r\n  noparse: $noparse \r\n}, \r\n{\r\n" : "  \r\n}, \r\n{\r\n";
+		$vkontakte .= "  type: '$vk_type',\r\n";      
+		$vkontakte .= "  text: '$vk_text'\r\n}));";
+		$vkontakte .= "\r\n-->\r\n</script></div>\r\n";
+
+
+	$array_buttons = array();
+	$temp = array();
+
+	$array_buttons = array('facebook'=>$facebook,'googlebuzz'=>$googlebuzz, 'googleplus'=>$googleplus, 'livejournal'=>$livejournal,'mailru'=>$mailru,'odnoklassniki'=>$odkl,'twitter'=>$twitter,'vkontakte'=>$vkontakte,'yandex'=>$yandex);
+
+	$btnsort=0;
+	$btnsort = get_option('buttons_sort');
+	$btnsort = explode(',',$btnsort);
+
+	$show = get_option('buttons_show');
+	if(@array_key_exists(0, $show)==false) {
+		$show = explode(',',$show);
+	}
+
+	asort($btnsort);
+
+	$btnsort=array_flip($btnsort);
+
+	for($i=0;$i<count($this->social_name);$i++) {
+
+		if(!in_array($this->social_name[$i],$show)) {
+			unset($array_buttons[$this->social_name[$i]]);
+			unset($btnsort[$this->social_name[$i]]);
+		}
+	}
+	$c=array_combine($btnsort,$array_buttons);
+	ksort($c);
+
+	$button_code.=implode('',$c);
+?>
